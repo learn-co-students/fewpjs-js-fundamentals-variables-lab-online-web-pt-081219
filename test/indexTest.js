cbff25d0cb14998ebaf4ab3bc054347de114826b
Alec Hall <alecnhall@gmail.com>
2020-02-28 14:08:08 -0700

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -14,7 +14,7 @@ describe('index.js', function () {
     it('is defined as a const', function () {
       expect(js).to.match(/const companyName/, "Expected companyName to be a const");
     });
-  });
+  }); 
 
   describe('mostProfitableNeighborhood', function () {
     it('is declared as equal to Chelsea', function () {
